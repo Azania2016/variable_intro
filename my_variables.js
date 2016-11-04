8ea9e720ac40f812495e2c3e19e7be387f3d6ca5
Azania2016 <Buzanimgamlele@gmail.com>
2016-11-04 11:02:33 +0200

    https://github.com/Azania2016/variable_intro.git

---
+++ my_variables.js
@@ -0,0 +1,16 @@
+var sunshine = true
+var age = 40;
+var surname = "Mgamlele"
+console.log(sunshine);
+console.log(typeof sunshine);
+console.log(age);
+console.log(typeof age);
+console.log(surname);
+console.log(typeof surname);
+
+var date = new Date();
+var fruit = ["aple,banana"];
+console.log(date);
+console.log(typeof date);
+console.log(fruit);
+console.log(typeof fruit);
